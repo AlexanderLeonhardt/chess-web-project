c73d6e109311de7e523069eff699bb6ab59bc5b4
Alex <alexpleonhardt@gmail.com>
2024-09-23 12:31:04 -0400

    king and queen moves

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -1,7 +1,11 @@
-import { getBishopMoves, getKnightMoves, getRookMoves } from "./getMoves";
+import { getBishopMoves, getKingMoves, getKnightMoves, getQueenMoves, getRookMoves } from "./getMoves";
 
 const arbiter = {
   getRegularMoves: function({position, piece, rank, file}) {
+    if (piece.endsWith('K'))
+      return getKingMoves({position, piece, rank, file});
+    if (piece.endsWith('Q'))
+      return getQueenMoves({position, piece, rank, file});
     if (piece.endsWith('B'))
       return getBishopMoves({position, piece, rank, file});
     if (piece.endsWith('N'))
